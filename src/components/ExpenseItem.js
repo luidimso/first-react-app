2bc04e9ffe53bae4be7dd4c3caeb65da9839b6da
luidimso <luidi_mso@outlook.com>
2021-07-16 17:53:37 -0300

    Adding dynamic data on HTML of Expensive Item component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+    const expenseDate = new Date();
+    const expenseTitle = "Car Insurance";
+    const expenseAmount = 200.50;
+
     return (
         <div className="expense-item">
-            <div>July 16th 2021</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">R$200,00</div>
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">R${expenseAmount}</div>
             </div>
         </div>
     );
