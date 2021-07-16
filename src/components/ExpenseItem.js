4cdf6524a9fcc5e2fa1f95fbda39296ea1ba6f5e
luidimso <luidi_mso@outlook.com>
2021-07-16 17:30:53 -0300

    Updating Expense Item component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-    return <h2>Expense Item Component</h2>;
+    return (
+        <div>
+            <div>July 16th 2021</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>R$200,00</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
