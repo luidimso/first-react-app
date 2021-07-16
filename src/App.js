66653ca7e0f1da1c293fa1f3d37aa6e557306ceb
luidimso <luidi_mso@outlook.com>
2021-07-16 18:11:44 -0300

    Passing properties to Expensive Item component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: '1',
+      title: 'Car 1',
+      amount: 200.50,
+      date: new Date()
+    },
+    {
+      id: '2',
+      title: 'Car 2',
+      amount: 200.50,
+      date: new Date()
+    },
+    {
+
+      id: '3',
+      title: 'Car 3',
+      amount: 200.50,
+      date: new Date()
+    },
+    {
+
+      id: '4',
+      title: 'Car 4',
+      amount: 200.50,
+      date: new Date()
+    }
+  ];
+
   return (
     <div>
       <h2>Testing</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
+      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-    const expenseDate = new Date();
-    const expenseTitle = "Car Insurance";
-    const expenseAmount = 200.50;
-
+function ExpenseItem(props) {
     return (
         <div className="expense-item">
-            <div>{expenseDate.toISOString()}</div>
+            <div>{props.date.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">R${expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">R${props.amount}</div>
             </div>
         </div>
     );
