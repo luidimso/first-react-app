df02ed229d0fab039e4c74ddf06a1605c151abd3
luidimso <luidi_mso@outlook.com>
2021-07-22 14:39:17 -0300

    Updating text on page

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,15 +3,17 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 function ExpenseItem(props) {
+    let title = props.title;
+
     function clickHandler() {
-        console.log("test");
+        title = "Updated";
     }
 
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}></ExpenseDate>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">R${props.amount}</div>
             </div>
             <button onClick={clickHandler}>Change Title</button>
