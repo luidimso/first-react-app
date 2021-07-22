8512d7f3df885b06d5a75434c7777d8e79e0c4ed
luidimso <luidi_mso@outlook.com>
2021-07-22 14:32:20 -0300

    Event handler on HTML elements

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,10 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 function ExpenseItem(props) {
+    function clickHandler() {
+        console.log("test");
+    }
+
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}></ExpenseDate>
@@ -10,6 +14,7 @@ function ExpenseItem(props) {
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">R${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
