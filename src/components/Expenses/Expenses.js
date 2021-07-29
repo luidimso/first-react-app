6f5e4baab9d0a31e3b80cfbbd6497358a860ec09
luidimso <luidi_mso@outlook.com>
2021-07-29 16:17:33 -0300

    Filtering expenses list using ExpenseFilter component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,16 +6,26 @@ import React, {useState} from "react";
 
 function Expenses(props) {
     const [filteredYear, setFilteredYear] = useState('2021');
+    const [expenses, setExpenses] = useState(props.expenses);
     
     function updateFilter(filter) {
         setFilteredYear(filter);
+        updateExpenses(filter);
+    }
+
+    function updateExpenses(filter) {
+        setExpenses(prevExpenses => {
+            return props.expenses.filter(expense => {
+                return expense.date.getFullYear().toString() === filter;
+            });
+        });
     }
 
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onFilterUpdated={updateFilter}></ExpensesFilter>
             {
-                props.expenses.map(expense => (<ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}></ExpenseItem>))
+                expenses.map(expense => (<ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}></ExpenseItem>))
             }
         </Card>
     );
