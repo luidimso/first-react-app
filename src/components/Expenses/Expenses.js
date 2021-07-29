99f5d9dafcedd33e95de28304879b6556c988e9b
luidimso <luidi_mso@outlook.com>
2021-07-29 15:21:59 -0300

    Using javascrip array map to render Expense component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,10 +14,9 @@ function Expenses(props) {
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onFilterUpdated={updateFilter}></ExpensesFilter>
-            <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date}></ExpenseItem>
-            <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date}></ExpenseItem>
-            <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date}></ExpenseItem>
-            <ExpenseItem title={props.expenses[3].title} amount={props.expenses[3].amount} date={props.expenses[3].date}></ExpenseItem>
+            {
+                props.expenses.map(expense => (<ExpenseItem title={expense.title} amount={expense.amount} date={expense.date}></ExpenseItem>))
+            }
         </Card>
     );
 }
